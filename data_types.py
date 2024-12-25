f189a7ca68061cd78a85e0d67e73a2ee8193e422
krisp09 <krishnascm21@gmail.com>
2024-12-25 17:02:14 +0530

    adding dataTypes file

---
+++ data_types.py
@@ -0,0 +1,30 @@
+def main():
+    #String (str)
+    instance_type = "t2.micro"
+    message = "My instance type is : "
+
+    #Integer
+    NumberofInstances = 5
+    hours_running = 10
+
+    #Bollean 
+    Instances_running = True
+
+    print (f"Are my instances running  {Instances_running}")
+    print (f"how many instances are running {NumberofInstances} hours of running {hours_running}")
+
+    print(f"showing type of variable boolean: {type(Instances_running)}")
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
